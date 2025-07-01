7910ca5279f931433bc7515a6e88bc0666ee6021
Sasha <a.chigrina89@gmail.com>
2025-07-02 01:02:32 +0300

    add Payment model

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from lms.models import Course, Lesson
+
 
 class User(AbstractUser):
     username = None
@@ -22,3 +24,24 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class Payment(models.Model):
+    PAYMENT_METHOD_CHOICES = [
+        ("cash", "Наличные"),
+        ("transfer", "Перевод")
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Пользователь", help_text="Пользователь, совершивший оплату", related_name="payments")
+    payment_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата оплаты", help_text="Дата и время совершения оплаты")
+    paid_course = models.ForeignKey(Course, on_delete=models.CASCADE, blank=True, null=True, verbose_name="Оплаченный курс", related_name="payments", help_text="Ссылка на курс, если он оплачен")
+    paid_lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, blank=True, null=True, verbose_name="Оплаченный урок", related_name="payments", help_text="Ссылка на урок, если он оплачен")
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Сумма оплаты", help_text="Сумма, уплаченная за курс или урок")
+    method = models.CharField(max_length=50, choices=PAYMENT_METHOD_CHOICES, verbose_name="Способ оплаты", help_text="Выберите способ оплаты: наличные или перевод")
+
+    class Meta:
+        verbose_name = "Платёж"
+        verbose_name_plural = "Платежи"
+
+    def __str__(self):
+        return f"{self.user.email} - {self.amount} ₽ ({self.payment_date.date()}) "
--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,13 @@
 from rest_framework.routers import SimpleRouter
 
 from users.apps import UsersConfig
-from users.views import UserViewSet
-
+from users.views import UserViewSet, PaymentViewSet
 
 app_name = UsersConfig.name
 
 router = SimpleRouter()
-router.register(r"", UserViewSet)
+router.register(r"users", UserViewSet)
+router.register(r"payments", PaymentViewSet)
 
 urlpatterns = [
 
