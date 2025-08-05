fef7c771d5d32d49dc2fb46c9188a807087d25bd
Sasha <a.chigrina89@gmail.com>
2025-08-05 08:50:21 +0300

    edit SECRET_KEY settings

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-for-ci-and-local")
 
 DEBUG = True if os.getenv("DEBUG") == "True" else False
 
