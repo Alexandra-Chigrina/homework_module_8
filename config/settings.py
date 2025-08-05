0f23e19043360846f27ec48c5239f1068f63d6cc
Sasha <a.chigrina89@gmail.com>
2025-08-05 03:29:00 +0300

    add ci.yml (pipeline for GitHub Actions)

--- config/settings.py
+++ config/settings.py
@@ -76,6 +76,13 @@ DATABASES = {
     }
 }
 
+if os.getenv('USE_SQLITE') == 'true':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'test_db.sqlite3',
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
