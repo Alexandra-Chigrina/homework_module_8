6ff46af6649bc3e3d6dd1e77e28995729532b009
Sasha <a.chigrina89@gmail.com>
2025-08-05 04:02:52 +0300

    edit README.md

--- config/settings.py
+++ config/settings.py
@@ -76,11 +76,11 @@ DATABASES = {
     }
 }
 
-if os.getenv('USE_SQLITE') == 'true':
+if os.getenv("USE_SQLITE") == "true":
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'test_db.sqlite3',
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "test_db.sqlite3",
         }
     }
 
@@ -110,7 +110,7 @@ USE_TZ = True
 
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
